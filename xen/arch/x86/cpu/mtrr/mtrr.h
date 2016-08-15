133da9756fb9d05756e7fe7041e40087db8a295e
Doug Goldstein <cardoe@cardoe.com>
2016-08-15 16:20:42 -0400

    x86/mtrr: drop unused func prototypes and struct
    These weren't used so drop them.
    
    Signed-off-by: Doug Goldstein <cardoe@cardoe.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>

--- xen/arch/x86/cpu/mtrr/mtrr.h
+++ xen/arch/x86/cpu/mtrr/mtrr.h
@@ -22,18 +22,6 @@ void mtrr_generic_set(unsigned int reg, unsigned long base,
         unsigned long size, mtrr_type type);
 int mtrr_generic_have_wrcomb(void);
 
-/* library functions for processor-specific routines */
-struct set_mtrr_context {
-	unsigned long flags;
-	unsigned long cr4val;
-	uint64_t deftype;
-	u32 ccr3;
-};
-
-void set_mtrr_done(struct set_mtrr_context *ctxt);
-void set_mtrr_cache_disable(struct set_mtrr_context *ctxt);
-void set_mtrr_prepare_save(struct set_mtrr_context *ctxt);
-
 void get_mtrr_state(void);
 
 extern u64 size_or_mask, size_and_mask;
@@ -41,6 +29,3 @@ extern u64 size_or_mask, size_and_mask;
 extern unsigned int num_var_ranges;
 
 void mtrr_state_warn(void);
-
-extern int amd_init_mtrr(void);
-extern int cyrix_init_mtrr(void);
